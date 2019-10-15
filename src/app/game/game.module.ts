567be35c51abf66650b1c57a588fa6de393cab81
katerynabugaieva <sonnensmile@gmail.com>
2019-10-15 09:56:05 +0200

    agg game component

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlayerService } from './services/player.service';
 import { GameService } from './services/game.service';
+import { GameComponent } from './game.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [GameComponent],
   imports: [
     CommonModule
   ],
+  exports:[GameComponent],
   providers: [GameService, PlayerService]
 })
 export class GameModule { }
